ff217ade7b90c25f736b136db083239cbaf545e7
zunerakamran <zunairakamran000@gmail.com>
2026-09-07 21:22:48 +0500

    Basic functionality added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
-
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
